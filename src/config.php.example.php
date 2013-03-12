374e17e9f4ac242aa52b79c157c68084c7bb9146
JCloudYu <j.cloud.yu@gmail.com>
2013-03-12 10:34:35 +0800

    [2013.03.12] Incorrect inclusion path fixed

--- src/config.php.example.php
+++ src/config.php.example.php
@@ -5,7 +5,7 @@
  * DateTime: 13/3/12 AM10:18
  */
 
-if(preg_match('/^win/', PHP_OS) === 1) define('__OS__', 'WIN');
+if(preg_match('/(^win|^WIN)/', PHP_OS) === 1) define('__OS__', 'WIN');
 else define('__OS__', 'UNIX');
 
 if(__OS__ == 'WIN')
