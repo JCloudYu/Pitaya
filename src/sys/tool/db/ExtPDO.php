3fdd99821753515612962f772fb84e59f52a1711
JCloudYu <j.cloud.yu@gmail.com>
2013-06-09 03:14:07 +0800

    [2013.06.09] ExtPDO now has the capability to create and manipulate the table which is used to store super global variables.

--- src/sys/tool/db/ExtPDO.php
+++ src/sys/tool/db/ExtPDO.php
@@ -2,11 +2,39 @@
 
 class ExtPDO extends PDO
 {
+	const VARIABLE_TABLE = '__extpdo_syswide_variables';
+
+	private $__use_Variable = FALSE;
+
 	public function __construct($dsn, $username, $userpass, $option = array()) {
+
+		$forceVar = ($option['CREATE_VAR']) ? TRUE : FALSE;
+		unset($option['CREATE_VAR']);
+
 		if (count($option) > 0)
 			parent::__construct($dsn, $username, $userpass, $option);
 		else
 			parent::__construct($dsn, $username, $userpass);
+
+		$this->__use_Variable = $this->__checkVariableCap($forceVar);
+	}
+
+	private function __checkVariableCap($forceVariable) {
+
+		$stmt = $this->select("SHOW TABLES;");
+		while (($row = $stmt->fetch(PDO::FETCH_NUM)) !== FALSE)
+			if ($row[0] === ExtPDO::VARIABLE_TABLE) return TRUE;
+
+		if ($forceVariable)
+		{
+			return $this->query("CREATE TABLE IF NOT EXISTS `" . ExtPDO::VARIABLE_TABLE . "`(`id` int(11) NOT NULL AUTO_INCREMENT,
+								`name` varchar(255) NOT NULL,
+						 		`value` blob,
+
+						 		PRIMARY KEY (`id`),
+						 		UNIQUE KEY `name_UNIQUE` (`name`));"
+			);
+		}
 	}
 
 	public function select($sqlStmt, $stmtVars = NULL) {
@@ -14,6 +42,8 @@ class ExtPDO extends PDO
 		if ($stmtVars)
 		{
 			$pdoStmt = parent::prepare($sqlStmt);
+			if ($pdoStmt === FALSE) return FALSE;
+
 			$pdoStmt->execute($stmtVars);
 
 			return $pdoStmt;
@@ -27,11 +57,14 @@ class ExtPDO extends PDO
 		if ($stmtVars)
 		{
 			$pdoStmt = parent::prepare($sqlStmt);
+			if ($pdoStmt === FALSE) return FALSE;
+
 			$pdoStmt->execute($stmtVars);
 		}
 		else
 		{
 			$pdoStmt = parent::query($sqlStmt);
+			if ($pdoStmt === FALSE) return FALSE;
 		}
 
 		return $pdoStmt->rowCount();
@@ -42,13 +75,17 @@ class ExtPDO extends PDO
 		if ($stmtVars)
 		{
 			$pdoStmt = parent::prepare($sqlStmt);
-			$pdoStmt->execute($stmtVars);
+			if (!$pdoStmt) return NULL;
 
+			$pdoStmt->execute($stmtVars);
 			$fetchData = $pdoStmt->fetch();
 		}
 		else
 		{
-			$fetchData =  parent::query($sqlStmt)->fetch();
+			$pdoStmt = parent::query($sqlStmt);
+			if (!$pdoStmt) return NULL;
+
+			$fetchData = $pdoStmt->fetch();
 		}
 
 		return $fetchData ? $fetchData : NULL;
@@ -69,5 +106,37 @@ class ExtPDO extends PDO
 		}
 
 		return $fetchData;
+	}
+
+	public function __unset($name) {
+
+		static $table = ExtPDO::VARIABLE_TABLE;
+		$this->query("DELETE FROM `$table` WHERE `name` = '$name';");
+	}
+
+	public function __get($name) {
+
+		static $table = ExtPDO::VARIABLE_TABLE;
+		$row = $this->fetch("SELECT * FROM `$table` WHERE `name` = '$name'");
+
+
+		return ($row) ? json_decode($row['value'], TRUE) : NULL;
+	}
+
+	public function __set($name, $value) {
+
+		static $table = ExtPDO::VARIABLE_TABLE;
+
+		$value = json_encode($value);
+
+		$result = $this->query("UPDATE `$table` SET `value` = :value WHERE `name` = :name;",
+							   array(':name' => $name,
+									 ':value' => $value));
+
+		if ($result > 0) return $result;
+
+		return $this->query("INSERT INTO `$table`(`name`, `value`) VALUES(:name, :value);",
+							 array(':name' => $name,
+								   ':value' => $value));
 	}
 }
