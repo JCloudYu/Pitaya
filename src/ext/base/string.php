82e1cf814be6acb4098aa8a63810feab0916a4b5
JCloudYu <j.cloud.yu@gmail.com>
2013-07-28 01:19:17 +0800

    System built-in password check function is inserted

--- src/ext/base/string.php
+++ src/ext/base/string.php
@@ -1,4 +1,5 @@
 <?php
 
 	function CheckAccountSyntax($account) { return preg_match("/^[a-zA-Z0-9][a-zA-Z0-9._-]+$/i", $account) ? TRUE : FALSE; }
+	function CheckPasswordSyntax($password) { $pass = trim($password); return (strlen($pass) >= 8 && $password === $pass); }
 	function CheckEmailSyntax($email) { return (filter_var($email, FILTER_VALIDATE_EMAIL) !== FALSE); }
