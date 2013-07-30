372906a868155e67e532b8ece5f3f8cedb202be5
JCloudYu <j.cloud.yu@gmail.com>
2013-07-30 18:42:18 +0800

    Ajax response management module has been added

---
+++ src/modules/ajax.php
@@ -0,0 +1,38 @@
+<?php
+	using('kernel.core.PBModule');
+
+	class ajax extends PBModule
+	{
+		const AJAX_STATUS_ALERT 	=  1;
+		const AJAX_STATUS_NORMAL	=  0;
+		const AJAX_STATUS_ERROR 	= -1;
+
+		public function exec($param)
+		{
+			$ajaxReturn = array();
+
+			if (!is_array($param))
+			{
+				$ajaxReturn['status'] 	= self::AJAX_STATUS_NORMAL_END;
+				$ajaxReturn['msg']		= $param;
+			}
+			else
+			{
+				$ajaxReturn = (is_int(!@$param['status'])) ? intval($param['status']) : self::AJAX_STATUS_NORMAL_END;
+				$ajaxReturn = (@$param['msg']) ? $param['msg'] : '';
+
+				unset($param['status']); unset($param['msg']);
+
+				$ajaxReturn = array_merge($ajaxReturn, $param);
+			}
+
+			self::respondJSON($ajaxReturn);
+		}
+
+		public static function respondJSON($jsonData)
+		{
+			header("Content-type: " . MIME::JSON);
+			$response = json_encode($jsonData);
+			echo "$response";
+		}
+	}
