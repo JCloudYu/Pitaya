1ddeec26d00e20d846ef6b76e2547594375280a8
JCloudYu <j.cloud.yu@gmail.com>
2013-09-04 18:42:20 +0800

    html module content is improved.

--- src/modules/html.php
+++ src/modules/html.php
@@ -26,10 +26,10 @@
 
 			// INFO: Process JS
 			$js['prepend'] = implode("\r\n", $this->_js['prepend']);
-			$js['prepend'] = "<script type='text/javascript'>{$js['prepend']}</script>";
+			$js['prepend'] = (!empty($js['prepend'])) ? "<script type='text/javascript'>{$js['prepend']}</script>" : '';
 
 			$js['append']  = implode("\r\n", $this->_js['append']);
-			$js['append'] = "<script type='text/javascript'>{$js['append']}</script>";
+			$js['append'] = (!empty($js['append'])) ? "<script type='text/javascript'>{$js['append']}</script>" : '';
 
 			foreach ($this->_jsFiles as $filePath)
 				$js['file'] .= "<script type='text/javascript' src='{$filePath}'></script>\r\n";
@@ -37,7 +37,7 @@
 
 			// INFO: Process CSS
 			$css['inline'] = implode("\r\n", $this->_css);
-			$css['inline'] = "<style type='text/css'>{$css['inline']}</style>";
+			$css['inline'] = (!empty($css['inline'])) ? "<style type='text/css'>{$css['inline']}</style>" : '';
 
 			foreach ($this->_cssFiles as $filePath)
 				$css['file'] .= "<link href='{$filePath}' rel='stylesheet' />\r\n";
@@ -54,10 +54,10 @@
 						{$css['file']}
 						{$css['inline']}
 					</head>
-					<BODY>
+					<body>
 						{$param}
 						{$js['append']}
-					</BODY>
+					</body>
 				</HTML>
 HTML;
 
@@ -79,21 +79,23 @@ HTML;
 
 		public function addFile($name, $type)
 		{
+			$serviceName = __SERVICE__;
+
 			switch (strtolower($type))
 			{
 				case 'js':
-					$this->_jsFiles[] = "{$this->_basePath}/js/{$name}.js";
+					$this->_jsFiles[] = "/{$serviceName}/{$this->_baseRCPath}{$name}";
 					break;
 				case 'css':
-					$this->_jsFiles[] = "{$this->_basePath}/css/{$name}.css";
+					$this->_cssFiles[] = "/{$serviceName}/{$this->_baseRCPath}{$name}";
 					break;
 				default: break;
 			}
 		}
 		public function __get_jsFiles() { return $this->_jsFiles; }
 		public function __get_cssFiles() { return $this->_cssFiles; }
-		public function __set_jsFiles($value) { $this->addFile($value, 'js'); }
-		public function __set_cssFiles($value) { $this->addFile($value, 'css'); }
+		public function __set_jsFile($value) { $this->addFile($value, 'js'); }
+		public function __set_cssFile($value) { $this->addFile($value, 'css'); }
 
 
 		public function __get_rcPath() { return $this->_baseRCPath; }
