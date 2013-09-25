12149913d288025211792181baffb6933ded08ba
JCloudYu <j.cloud.yu@gmail.com>
2013-09-25 21:48:33 +0800

    System configuration example is modified

--- env/config.example.php
+++ env/config.example.php
@@ -1,8 +1,15 @@
 <?php
 
-	$GLOBALS['servicePath'] = (__OS__ == 'WIN') ? 'E:/path/to/service/folder' : '/path/to/service/folder';
+	// Example of the service path
+	// $GLOBALS['servicePath'] = (__OS__ == 'WIN') ? 'E:/path/to/service/folder' : '/path/to/service/folder';
+
+	// This is the default service path, you can assign customized path... with proper permissions
+	$GLOBALS['servicePath'] = __DIR__ . '/Services';
+
 
 	//define("__DEFAULT_SERVICE__", 'index', TRUE);
 	//define("__DEBUG_MODE__", TRUE, TRUE);
 	//define("__DEBUG_CONSOLE_WIDTH__", 350, TRUE);
+
+	// OS Dependent...
 	//define("__CASE_SENSITIVE_MODULE_NAME__", TRUE, TRUE);
