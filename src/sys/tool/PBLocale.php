24fedb2aa7094abb30524000fc3cdd5d1146d693
JCloudYu <j.cloud.yu@gmail.com>
2013-10-05 19:04:55 +0800

    PHP locale translation class is constructed.

---
+++ src/sys/tool/PBLocale.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * 0026.litchi - locale.php
+ * Created by JCloudYu on 2013/10/05 13:41
+ */
+
+	using('kernel.basis.PBObject');
+
+	final class PBLocale extends PBObject implements ArrayAccess
+	{
+		private static $_sharedBasePackage = NULL;
+
+		public static function basePackage($packagePath = NULL) { self::$_sharedBasePackage = "$packagePath"; }
+
+
+		private $_basePackage = '';
+		private $_curLocale = '';
+		private $_storedLocales = array();
+
+		public function __construct()
+		{
+			$this->_basePackage = self::$_sharedBasePackage;
+			$this->_curLocale = 'default';
+			$this->_storedLocales = array('default' => array());
+		}
+
+		public function __get_basePackage() { return $this->_basePackage; }
+		public function __set_basePackage($value) { $this->_basePackage = "$value"; }
+
+		public function __get_locale() { return $this->_curLocale; }
+		public function __set_locale($value)
+		{
+			$this->_curLocale =  empty($value) ? '' : "$value";
+
+			if (isset($this->_storedLocales[$this->_curLocale])) return;
+
+			$leading = empty($this->_basePackage) ? '' : "{$this->_basePackage}.";
+			$locale = Imprint("service.{$leading}{$this->_curLocale}.locale");
+			@$locale = array_key_exists('locale', $locale) ? $locale['locale'] : array();
+
+			$this->_storedLocales[$this->_curLocale] = $locale;
+
+			DEBUG::VarDump($this->_storedLocales);
+		}
+
+		public function offsetExists($offset) { return TRUE; }
+
+		public function offsetUnset($offset) { unset($this->_storedLocales[$this->_curLocale][$offset]); }
+
+		public function offsetSet($offset, $value) { $this->_storedLocales[$this->_curLocale][$offset] = $value; }
+		public function offsetGet($offset) { return strtr($offset, $this->_storedLocales[$this->_curLocale]); }
+	}
