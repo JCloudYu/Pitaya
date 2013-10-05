22239a9cc39e2454026d4a791681282658c54d2c
JCloudYu <j.cloud.yu@gmail.com>
2013-10-05 23:23:43 +0800

    Pseudo class of PBLocale is added

---
+++ proj/define.dummy/PBLocale.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 0026.litchi - locale.php
+ * Created by JCloudYu on 2013/10/05 13:41
+ */
+
+	using('kernel.basis.PBObject');
+
+	final class PBLocale extends PBObject implements ArrayAccess
+	{
+		public $basePackage = '';
+		public $locale = '';
+		public static function basePackage($packagePath = NULL) {}
+	}
