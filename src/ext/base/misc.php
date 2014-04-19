20590dce5bb72a03f965b4fdd2babbd74ddbd8f2
JCloudYu <j.cloud.yu@gmail.com>
2014-04-19 22:48:41 +0800

    Remove useless function ImprintFile

--- src/ext/base/misc.php
+++ src/ext/base/misc.php
@@ -105,31 +105,6 @@
 	}
 
 	/**
-	 * Returns the global variables defined in target source file
-	 *
-	 * @param string $____path_of_the_source_file_to_be_imprinted the targeted source file to be imprinted
-	 *
-	 * @return array|null returns array if the target file exists, null otherwise
-	 */
-	function ImprintFile($____path_of_the_source_file_to_be_imprinted = '')
-	{
-		$____pre_cached_to_be_deleted_existing_variables = array();
-		$____pre_cached_to_be_deleted_existing_variables = get_defined_vars();
-
-		if (!is_string($____path_of_the_source_file_to_be_imprinted) ||
-			 empty($____path_of_the_source_file_to_be_imprinted))
-			return NULL;
-
-		require $____path_of_the_source_file_to_be_imprinted;
-		$____path_of_the_source_file_to_be_imprinted = get_defined_vars();
-
-		foreach ($____pre_cached_to_be_deleted_existing_variables as $varName => $varValue)
-			unset($____path_of_the_source_file_to_be_imprinted[$varName]);
-
-		return $____path_of_the_source_file_to_be_imprinted;
-	}
-
-	/**
 	 * Returns the global variables defined in target package
 	 *
 	 * @param string $____path_of_the_package_file_to_be_imprinted the targeted package to be imprinted
