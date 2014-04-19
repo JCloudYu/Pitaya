4b12af3f0ee14429d08ea734ba08b93795828076
JCloudYu <j.cloud.yu@gmail.com>
2014-04-19 22:52:14 +0800

    Add PBPage and PBLayout for development of page modularization

---
+++ src/kernel/core/PBPage.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * 0005.danshen - PBPage.php
+ * Created by JCloudYu on 2014/04/19 22:00
+ */
+	using('kernel.core.PBModule');
+	using('ext.base.misc');
+	using('kernel.tool.html.PBLayout');
+
+	abstract class PBPage extends PBModule
+	{
+		private $_layoutPath	= '';
+		private $_layoutStuct	= array();
+		private $_layoutObj		= NULL;
+
+		public function __construct() { $this->_layoutObj = new PBLayout(); }
+
+		public function exec($param)
+		{
+			return $this->render($this->_layoutObj);
+		}
+
+		protected function __get_layoutPath() { return $this->_layoutPath; }
+		protected function __set_layoutPath($layoutPath)
+		{
+			if (!file_exists($layoutPath))
+			{
+				$this->_layoutPath  = '';
+				$this->_layoutStuct = array();
+				return;
+			}
+
+
+
+			$layout = json_decode(file_get_contents($layoutPath), TRUE);
+			if (!is_array($layout))
+			{
+				$this->_layoutPath  = '';
+				$this->_layoutStuct = array();
+				return;
+			}
+
+
+
+			$this->_layoutStuct = $layout;
+			$this->_layoutPath  = $layoutPath;
+			$this->_layoutObj->processLayout($this->_layoutStuct);
+		}
+
+		protected function __get_layout(){ return $this->_layoutStuct; }
+		protected function __set_layout($layoutStruct)
+		{
+			$this->_layoutPath = '';
+			$this->_layoutStuct = (!is_array($layoutStruct)) ? array() : $layoutStruct;
+			$this->_layoutObj->processLayout($this->_layoutStuct);
+		}
+
+		abstract function render($layout);
+	}
---
+++ src/kernel/tool/html/PBLayout.php
@@ -0,0 +1,60 @@
+<?php
+	/**
+	 * 0005.danshen - Layout.php
+	 * Created by JCloudYu on 2014/04/02 23:43
+	 */
+	using('kernel.core.PBModule');
+
+	class PBLayout implements ArrayAccess
+	{
+		private $_regions	 	= array();
+		private $_region_cache	= array();
+
+		public function __construct($layoutStruct = array()) { $this->processLayout($layoutStruct); }
+
+		public function processLayout($layoutStruct)
+		{
+			$process = PBProcess::Process();
+			$this->_regions = $this->_region_cache = array();
+
+			if (empty($layoutStruct)) return;
+
+			foreach ($layoutStruct as $regionName => $regionContent)
+			{
+				$this->_regions[$regionName] = array();
+				foreach ($regionContent as $moduleConf)
+				{
+					$module = $process->getModule($moduleConf['module'], FALSE);
+					$module->tag = md5($module->id);
+					$this->_regions[$regionName][] = $module;
+					$module->prepare($moduleConf['request']);
+				}
+			}
+		}
+
+		public function offsetGet($offset)
+		{
+			if (isset($this->_region_cache[$offset]))
+				return $this->_region_cache[$offset];
+
+			$resultCache = '';
+
+			if (!empty($this->_regions[$offset]))
+			{
+				foreach ($this->_regions[$offset] as $module)
+				{
+					$result = $module->exec(NULL);
+					$resultCache .= "<div modId='{$module->tag}' module='{$module->class}'>{$result}</div>";
+				}
+			}
+
+			$this->_region_cache[$offset] = $resultCache;
+			return $resultCache;
+		}
+
+
+
+		public function offsetSet($offset, $value)	{}
+		public function offsetUnset($offset)		{}
+		public function offsetExists($offset)		{}
+	}
