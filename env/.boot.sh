9c46865429d43d19c1094254edffbab46acc116b
JCloudYu <j.cloud.yu@gmail.com>
2014-12-11 05:16:44 +0800

    Fix error flag file name

--- env/.boot.sh
+++ env/.boot.sh
@@ -7,4 +7,4 @@ CURR_WD=$(pwd)
 BOOT_TIME=($(cat /proc/stat | grep btime))
 BOOT_TIME=${BOOT_TIME[1]}
 
-if [ -f ".b_flag" ] && [ $BOOT_TIME -lt "$(stat -c %Y .t-boot 2>&1)" ]; then exit 0; else touch ".b_flag"; fi
+if [ -f ".b_flag" ] && [ $BOOT_TIME -lt "$(stat -c %Y .b_flag 2>&1)" ]; then exit 0; else touch ".b_flag"; fi
