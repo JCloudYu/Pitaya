79dfa9608434af66d0f05852fa5b8a2e3b3287a1
JCloudYu <j.cloud.yu@gmail.com>
2014-12-12 17:01:30 +0800

    1. Modify bash portal to accept additional arguments 2. Add entry script sample pitaya.sh

--- src/portal.sh
+++ src/portal.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+EXT_ARGUMENTS=(${ENV_ARGUMENTS})
+
 cd $(dirname $0)
-php -d auto_prepend_file=kernel/base.php portal.php $@
+php -d auto_prepend_file=kernel/base.php ${EXT_ARGUMENTS[@]} portal.php $@
