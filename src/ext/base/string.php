3fefa58a46ee66c9394589e190815fed5b47549b
JCloudYu <j.cloud.yu@gmail.com>
2015-04-17 18:42:34 +0800

    Prevent PHP from casting TRUE value to string '1' in ext_strtr api

--- src/ext/base/string.php
+++ src/ext/base/string.php
@@ -107,9 +107,10 @@
 	function ext_strtr($pattern, $replacements, $glue = FALSE, $mapper = NULL)
 	{
 		// INFO: Fail safe
-		if ( !is_array($replacements) ) return "";
+		if ( !is_array($replacements) ) return '';
 		$mapper	 = ( !is_callable($mapper) ) ? function($item){ return $item; } : $mapper;
 		$pattern = "{$pattern}";
+		$glue	 = ( $glue === TRUE ) ? '' : $glue;
 
 
 		$firstElm = reset($replacements);
