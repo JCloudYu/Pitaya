a41567eaea6cf8b1884322bf197b0db79fa04837
JCloudYu <j.cloud.yu@gmail.com>
2015-05-26 21:22:42 +0800

    Add newer examples to the system configuration files

---
+++ env/common.example.php
@@ -0,0 +1,9 @@
+<?php
+//	$GLOBALS['extPath']['tmp'] = '/tmp';
+
+//	define("__SYSTEM_TIMEZONE__",		"UTC",			TRUE);
+//	define("__DEBUG_MODE__",			FALSE,			TRUE);
+//	define("__LOG_EXCEPTION__",			TRUE,			TRUE);
+//	define("__THROW_EXCEPTION__",		FALSE,			TRUE);
+
+//	define("__DEBUG_CONSOLE_WIDTH__",	200,			TRUE);
