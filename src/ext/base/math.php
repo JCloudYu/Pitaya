0bd2e08c187a8504bf76642804688627ff97c870
JCloudYu <j.cloud.yu@gmail.com>
2015-09-15 12:04:18 +0800

    Fix invalid regular expression repetition syntax

--- src/ext/base/math.php
+++ src/ext/base/math.php
@@ -8,7 +8,7 @@
 
 
 	function EXPR_INT($val) 			{ return (preg_match('/^[-+]{0,1}\d+$/', "{$val}") > 0); }
-	function EXPR_UINT($val) 			{ return (preg_match('/^{0,1}\d+$/', "{$val}") > 0); }
+	function EXPR_UINT($val) 			{ return (preg_match('/^\d+$/', "{$val}") > 0); }
 	function EXPR_FLOAT($val)			{ return EXPR_FLOAT_DOT($val) || EXPR_FLOAT_SCIENCE($val); }
 	function EXPR_FLOAT_DOT($val)		{ return (preg_match('/^[-+]{0,1}((\d*\.\d+)|(\d+\.\d*))$/', "{$val}") > 0); }
 	function EXPR_FLOAT_SCIENCE($val)
