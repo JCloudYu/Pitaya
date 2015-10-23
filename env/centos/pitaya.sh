59776f016b1653681093b5468838a71013e0c358
JCloudYu <j.cloud.yu@gmail.com>
2015-10-24 04:42:24 +0800

    1. Add global pitaya command 2. Leave pitaya.sh as it was

--- env/centos/pitaya.sh
+++ env/centos/pitaya.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-PITAYA_DIR="${PITAYA_LIB_PATH}";
+
+cd $(dirname "$0");
 WORKING_FOLDER=$( pwd );
 
 
@@ -31,46 +32,11 @@ function STDERR { cat - 1>&2; }
 mod=$1;
 
 if [ "${mod}" == "-c" ]; then
-
-	## Check Pitaya Library Path
-	if [ "${PITAYA_DIR}" == "" ]; then
-		echo "PITAYA_LIB_PATH environmental variable is not defined!" | STDERR;
-		exit 1;
-	fi;
-
-
 	shift;
 	command=$1;
 	case "${command}" in
-		"create" )
-			shift;
-			if [ ! -e "./Pitaya" ]; then mkdir Pitaya; fi;
-			if [ ! -e "./Services" ]; then mkdir Services; fi;
-			if [ ! -e "./Share" ]; then mkdir Share; fi;
-			find "${PITAYA_DIR}/env" -type f -not -name "pitaya.sh" -exec cp \{\} ./ \;
-			if [ -e "cli.example.php" ]; then mv "cli.example.php" "cli.php"; fi;
-			if [ -e "config.example.php" ]; then mv "config.example.php" "config.php"; fi;
-			if [ -e "common.example.php" ]; then mv "common.example.php" "common.php"; fi;
-			if [ -e "config-host.example.php" ]; then
-				if [ "$1" == "--host-base" ] || [ "$1" == "-h" ]; then shift;
-					mv "config-host.example.php" "config-host.php";
-				else
-					rm "config-host.example.php";
-				fi;
-			fi;
-
-			echo -e "\n\nmount -o bind \"${PITAYA_DIR}/src\" \"./Pitaya\"\n" >> "./.boot.sh";
-			exit 0;
-		;;
-
 		"unmount" )
 			UNMOUNT_PROJ
-			exit 0;
-		;;
-
-		"destroy" )
-			UNMOUNT_PROJ
-			find "${WORKING_FOLDER}" -mindepth 1 -maxdepth 1 -not -name "pitaya.sh" -not -name "." -not -name ".." -exec rm -rf \{\} \;
 			exit 0;
 		;;
 
@@ -79,7 +45,7 @@ if [ "${mod}" == "-c" ]; then
 				echo "${command} is not a valid command!" | STDERR;
 			fi;
 
-			echo "Supported commands are create, unmount, and destroy!" | STDERR;
+			echo "Supported commands are unmount" | STDERR;
 
 			exit 1;
 		;;
