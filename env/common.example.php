7ebafb964f54faf203fdf07c187c8d2f0895b343
JCloudYu <j.cloud.yu@gmail.com>
2015-10-23 22:52:54 +0800

    1. Remove comments in sample configuration files 2. Change default value in cli configuration file cli.php

--- env/common.example.php
+++ env/common.example.php
@@ -1,9 +1,9 @@
 <?php
-//	$GLOBALS['extPath']['tmp'] = '/tmp';
+	$GLOBALS['extPath']['tmp'] = '/tmp';
 
-//	define("__SYSTEM_TIMEZONE__",		"UTC",			TRUE);
-//	define("__DEBUG_MODE__",			FALSE,			TRUE);
-//	define("__LOG_EXCEPTION__",			TRUE,			TRUE);
-//	define("__THROW_EXCEPTION__",		FALSE,			TRUE);
+	define("__SYSTEM_TIMEZONE__",		"UTC",			TRUE);
+	define("__DEBUG_MODE__",			TRUE,			TRUE);
+	define("__LOG_EXCEPTION__",			TRUE,			TRUE);
+	define("__THROW_EXCEPTION__",		FALSE,			TRUE);
 
-//	define("__DEBUG_CONSOLE_WIDTH__",	200,			TRUE);
+	define("__DEBUG_CONSOLE_WIDTH__",	350,			TRUE);
--- env/config-host.example.php
+++ env/config-host.example.php
@@ -1,7 +1,7 @@
 <?php
-//	$GLOBALS['servicePath']		= __DIR__ . '/Services';
-//	$GLOBALS['sharePath']		= __DIR__ . '/Share';
-//	$GLOBALS['dataPath']		= __DIR__ . '/Data';
-//	$GLOBALS['extPath']			= array();
+	$GLOBALS['servicePath']		= __DIR__ . '/Services';
+	$GLOBALS['sharePath']		= __DIR__ . '/Share';
+	$GLOBALS['dataPath']		= __DIR__ . '/Data';
+	$GLOBALS['extPath']			= array();
 
-//	define( 'DEFAULT_SERVICE', 'index', TRUE );
+	define( 'DEFAULT_SERVICE', 'index', TRUE );
