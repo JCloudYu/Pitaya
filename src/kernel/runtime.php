7e3c15ba45828e4e33ceae2a7171b48a18e411d8
JCloudYu <j.cloud.yu@gmail.com>
2015-10-27 02:24:37 +0800

    Add PITAYA_SYSTEM_TIMEZONE environmental variable support

--- src/kernel/runtime.php
+++ src/kernel/runtime.php
@@ -271,17 +271,20 @@
 		$result = FALSE;
 
 
-		if ( isset( $GLOBALS['RUNTIME_CONF']['TZ'] ) )
-			$result = date_default_timezone_set( $GLOBALS['RUNTIME_CONF']['TZ'] );
-		else
 		if ( defined('__SYSTEM_TIMEZONE__') )
 			$result = date_default_timezone_set( __SYSTEM_TIMEZONE__ );
 		else
-		if ( ini_get('date.timezone') != "" )
-			$result = date_default_timezone_set( ini_get('date.timezone') );
+		if ( isset( $GLOBALS['RUNTIME_CONF']['TZ'] ) )
+			$result = date_default_timezone_set( $GLOBALS['RUNTIME_CONF']['TZ'] );
+		else
+		if ( isset($GLOBALS['RUNTIME_ENV']['PITAYA_SYSTEM_TIMEZONE']) )
+			$result = date_default_timezone_set( $GLOBALS['RUNTIME_ENV']['PITAYA_SYSTEM_TIMEZONE'] );
 		else
 		if ( isset($GLOBALS['RUNTIME_ENV']['TZ']) )
 			$result = date_default_timezone_set( $GLOBALS['RUNTIME_ENV']['TZ'] );
+		else
+		if ( ini_get('date.timezone') != "" )
+			$result = date_default_timezone_set( ini_get('date.timezone') );
 
 
 		if ( $result === FALSE ) date_default_timezone_set('UTC');
