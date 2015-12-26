1a5ca9f3f786cf38da67f210d29a1f6ee375c42c
JCloudYu <j.cloud.yu@gmail.com>
2015-12-27 04:27:42 +0800

    Add PBTaskWorker and PBTaskKernel for batched regular tasks execution

---
+++ src/sys/tool/PBTaskWorker/PBTaskWorker.php
@@ -0,0 +1,32 @@
+<?php
+	/**
+	 ** Pitaya - PBTaskWorker.php
+	 ** Created by JCloudYu on 2015/11/20 09:55
+	 **/
+	using( 'kernel.core.PBModule' );
+	using( 'sys.tool.PBTaskWorker.PBTaskKernel' );
+
+	class PBTaskWorker extends PBObject
+	{
+		private $_seqNum	= 0;
+		private $_kernels	= array();
+		public function addTaskKernel( PBTaskKernel $task ) {
+			if ( in_array($task, $this->_kernels, TRUE) ) return;
+			$task->seqId = ++$this->_seqNum;
+			$this->_kernels[ $this->_seqNum ] = $task;
+		}
+
+		public function process( $msg = NULL ) {
+			$result = array();
+			foreach ( $this->_kernels as $idx => $task )
+				$result[$idx] = $task->doProcess( $msg );
+			return $result;
+		}
+
+		public function batch( $msgPackages ) {
+			$results = array();
+			foreach ( $msgPackages as $key => $msgContent )
+				$results[ $key ] = $this->process( $msgContent );
+			return $results;
+		}
+	}
