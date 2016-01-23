34b362534adf89f0438918a7775671f40d697f11
JCloudYu <j.cloud.yu@gmail.com>
2016-01-23 10:04:20 +0800

    1. Add gateway redirection script for windows environment 2. Rewrite base.php according to contents of gateway.php

---
+++ env/http/gateway.php
@@ -0,0 +1,3 @@
+<?php
+	define( '__ROOT__', dirname( __FILE__ ) . "/Pitaya", FALSE );
+	require_once __ROOT__ . "/portal.php";
--- src/kernel/base.php
+++ src/kernel/base.php
@@ -60,10 +60,14 @@
 
 
 	// INFO: Execution environment
+	if ( !defined( '__ROOT__' ) )
+		define('__ROOT__', realpath( dirname($_SERVER["SCRIPT_FILENAME"]) ), TRUE);
+
+
 	if ( php_sapi_name() == "cli" )
 	{
 		define('SYS_WORKING_ENV',	SYS_ENV_CLI, TRUE); // DEPRECATED: The constants will be removed in v1.4.0
-		define('__ROOT__',			realpath( dirname($_SERVER["SCRIPT_FILENAME"]) ), TRUE);
+
 		define('SYS_EXEC_ENV',		EXEC_ENV_CLI, TRUE);
 		define('PITAYA_HOST',		 @"{$GLOBALS['RUNTIME_ENV']['PITAYA_HOST']}", TRUE);
 		define('EOL',				"\n", TRUE);
@@ -91,7 +95,6 @@
 	}
 	else
 	{
-		define('__ROOT__', realpath( dirname($_SERVER["SCRIPT_FILENAME"]) ), TRUE);
 		define('SYS_EXEC_ENV',	  EXEC_ENV_HTTP, TRUE);
 		define('PITAYA_HOST', "{$_SERVER['HTTP_HOST']}", TRUE);
 
