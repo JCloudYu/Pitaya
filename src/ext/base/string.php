1a5b2397bbf51dc7adedf4aed0dc7ad6033c08c9
JCloudYu <j.cloud.yu@gmail.com>
2016-02-11 15:45:57 +0800

    1. Add base32_encode / base32_decode APIs 2. Add PBCrypto::GenTOTP / PBCrypto::GenHOTP APIs 3. Minor modifications

--- src/ext/base/string.php
+++ src/ext/base/string.php
@@ -116,6 +116,64 @@
 		}
 	}
 
+	function base32_encode( $input )
+	{
+		$BASE32_ALPHABET = 'abcdefghijklmnopqrstuvwxyz234567';
+		$output          = '';
+		$v               = 0;
+		$vbits           = 0;
+
+		for ( $i = 0, $j = strlen( $input ); $i < $j; $i++ ) {
+			$v <<= 8;
+			$v += ord( $input[ $i ] );
+			$vbits += 8;
+
+			while ( $vbits >= 5 ) {
+				$vbits -= 5;
+				$output .= $BASE32_ALPHABET[ $v >> $vbits ];
+				$v &= ( ( 1 << $vbits ) - 1 );
+			}
+		}
+
+		if ( $vbits > 0 ) {
+			$v <<= ( 5 - $vbits );
+			$output .= $BASE32_ALPHABET[ $v ];
+		}
+
+		return $output;
+	}
+
+	function base32_decode( $input )
+	{
+		$input = strtolower($input);
+
+		$output = '';
+		$v      = 0;
+		$vbits  = 0;
+
+		for ( $i = 0, $j = strlen( $input ); $i < $j; $i++ ) {
+			$v <<= 5;
+			if ( $input[ $i ] >= 'a' && $input[ $i ] <= 'z' ) {
+				$v += ( ord( $input[ $i ] ) - 97 );
+			}
+			elseif ( $input[ $i ] >= '2' && $input[ $i ] <= '7' ) {
+				$v += ( 24 + $input[ $i ] );
+			}
+			else {
+				exit( 1 );
+			}
+
+			$vbits += 5;
+			while ( $vbits >= 8 ) {
+				$vbits -= 8;
+				$output .= chr( $v >> $vbits );
+				$v &= ( ( 1 << $vbits ) - 1 );
+			}
+		}
+
+		return $output;
+	}
+
 	// INFO: Extensions for UTF8 data processing
 	function utf8_filter( $string, $func = NULL )
 	{
--- src/kernel/basis/PBCrypto.php
+++ src/kernel/basis/PBCrypto.php
@@ -9,7 +9,7 @@
 		const CANDIDATES_MIXED_NO_SYM = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		const CANDIDATES_SYMBOLS	  = " !\"$%^&*()-_=+[{]};:'@#~|,<.>/?\\/`";
 
-		public static function GenPass($length, $caseSensitive = TRUE, $withSymbol = TRUE)
+		public static function GenPass( $length, $caseSensitive = TRUE, $withSymbol = TRUE )
 		{
 			$candidate = ($caseSensitive) ? self::CANDIDATES_MIXED_NO_SYM : self::CANDIDATES_LOWER_NO_SYM;
 			$candidate = str_split( ($withSymbol) ? $candidate . self::CANDIDATES_SYMBOLS : $candidate );
@@ -27,6 +27,31 @@
 			while( !self::ValidatePass($pass, $caseSensitive, $withSymbol) );
 
 			return $pass;
+		}
+
+		public static function GenTOTP( $raw_secret, $refTime = NULL, $length = 6, $alg = 'sha1', $timeQuantum = 30 ) {
+			$refTime = ( $refTime === NULL ) ? time() : $refTime;
+			return self::GenHOTP( $raw_secret, (($refTime * 1000) / ($timeQuantum * 1000)) | 0, $length, $alg );
+		}
+
+		public static function GenHOTP( $raw_secret, $counter, $length = 6, $alg = 'sha1' ) {
+			if ( PHP_VERSION_ID >= 50603 )
+				$counter = pack( 'J', $counter );
+			else
+				$counter = str_pad( pack( 'N', $counter ), 8, chr(0), STR_PAD_LEFT );
+
+			$hash = hash_hmac( $alg, $counter, $raw_secret, TRUE );
+			$hasLen = strlen( $hash );
+
+			$offset = ord($hash[ $hasLen - 1 ]) & 0xf;
+			$otp = (
+				((ord($hash[ $offset + 0 ]) & 0x7f) << 24 ) |
+				((ord($hash[ $offset + 1 ]) & 0xff) << 16 ) |
+				((ord($hash[ $offset + 2 ]) & 0xff) <<  8 ) |
+				 (ord($hash[ $offset + 3 ]) & 0xff)
+			) % pow( 10, $length );
+
+			return str_pad( $otp, $length, "0", STR_PAD_LEFT );
 		}
 
 		public static function ValidatePass($target, $caseSensitive = TRUE, $checkSymbol = TRUE)
