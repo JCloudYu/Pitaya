5cbbdf4a032dfc1402187fe003dac88fe8fad17d
JCloudYu <j.cloud.yu@gmail.com>
2016-02-12 18:35:09 +0800

    Add PBJSONXXX objects for ambiguous representation of PHP Arrays

--- src/ext/base/objects.php
+++ src/ext/base/objects.php
@@ -50,3 +50,104 @@
 
 		public function __toString() { return $this->breadcrumb(); }
 	}
+
+
+	abstract class PBJSONContainer implements ArrayAccess, Iterator
+	{
+		protected $_container = array();
+		protected $_end		  = TRUE;
+
+		abstract function safe_cast();
+
+		public function offsetSet( $offset, &$value ) {
+			if ( $offset === NULL )
+				$this->_container[] = $value;
+			else
+				$this->_container[ $offset ] = $value;
+		}
+		public function& offsetGet( $offset ) {
+			return $this->_container[ $offset ];
+		}
+		public function offsetExists( $offset ) {
+			return array_key_exists( $offset, $this->_container );
+		}
+		public function offsetUnset( $offset ) {
+			unset( $this->_container[ $offset ] );
+		}
+
+		public function& __get( $name ) {
+			return $this[$name];
+		}
+		public function __set( $name, &$value ) {
+			$this[ $name ] = $value;
+		}
+		public function __isset( $name ) {
+			return isset( $this[$name] );
+		}
+		public function __unset( $name ) {
+			unset( $this[$name] );
+		}
+
+		public function& current () {
+			return current($this->_container);
+		}
+		public function key() {
+			return key($this->_container);
+		}
+
+		public function next() {
+			$this->_end = !next($this->_container);
+		}
+
+		public function rewind() {
+			reset($this->_container);
+		}
+
+		public function valid() {
+			return ( count($this->_container) <= 0 ) ? FALSE : $this->_end;
+		}
+
+		public static function Flatten( $content ) {
+
+			if ( is_a( $content, 'stdClass' ) )
+			{
+				$result = new stdClass();
+				foreach ( $content as $key => $value )
+					$result->{$key} = self::Flatten($value);
+				return $result;
+			}
+			else
+			if ( is_array( $content ) )
+			{
+				foreach ( $content as $key => $value )
+					$content[$key] = self::Flatten($value);
+				return $content;
+			}
+			else
+			if ( is_a( $content, 'PBJSONContainer' ) )
+				return $content->safe_cast();
+
+
+			return $content;
+		}
+	}
+
+	class PBJSONObject extends PBJSONContainer
+	{
+		public function safe_cast() {
+			foreach ( $this->_container as $key => $value )
+				$this->_container[$key] = PBJSONContainer::Flatten( $value );
+
+			return (object)$this->_container;
+		}
+	}
+
+	class PBJSONArray extends PBJSONContainer
+	{
+		public function safe_cast() {
+			foreach ( $this->_container as $key => $value )
+				$this->_container[$key] = PBJSONContainer::Flatten( $value );
+				
+			return array_values($this->_container);
+		}
+	}
