95bfe45978098962dbcc6c7e8ca8e1aa4f8255b3
JCloudYu <j.cloud.yu@gmail.com>
2016-09-03 22:24:52 +0800

    Allow developers to control over logged tags

--- src/kernel/basis/PBLog.php
+++ src/kernel/basis/PBLog.php
@@ -8,6 +8,13 @@
 
 	final class PBLog
 	{
+		const LOG_INFO_TIME		= 1;
+		const LOG_INFO_CATE		= 2;
+		const LOG_INFO_SERVICE	= 4;
+		const LOG_INFO_MODULE	= 8;
+		const LOG_INFO_ROUTE	= 16;
+		const LOG_INFO_ALL		= 0xFFFFFFFF;
+	
 		private $_logStream = NULL;
 		public function __construct($logPath)
 		{
@@ -16,7 +23,8 @@
 		public function genLogMsg( $message, $logPos = FALSE, $logCate = '', $options = [] )
 		{
 			if ( !is_array($options) ) $options = [];
-
+			
+			$LOG_INFO = array_key_exists( 'info-level', $options ) ? $options['info-level'] | 0 : self::LOG_INFO_ALL;
 			if ( !is_string($message) ) $message = print_r( $message, TRUE );
 			if ( !is_array(@$options['tags']) ) $options['tags'] = array();
 			$info = self::PrepLogInfo($logCate, $options);
@@ -32,8 +40,22 @@
 
 			// INFO: Write file stream
 			$position = ($logPos) ? " {$info['position']}" : '';
-			$timeInfo = in_array('UNIX_TIMESTAMP', $options) ? $info['time'] : $info['timestamp'];
-			$msg = "[{$timeInfo}][{$info['cate']}][{$info['service']}][{$info['module']}][{$info['route']}]{$tags} {$message}{$position}";
+			
+			$timeInfo = '';
+			if ( $LOG_INFO & self::LOG_INFO_TIME )
+			{
+				$timeInfo = in_array( 'UNIX_TIMESTAMP', $options ) ? $info['time'] : $info['timestamp'];
+				$timeInfo = "[{$timeInfo}]";
+			}
+			
+			$cateInfo	= ( $LOG_INFO & self::LOG_INFO_CATE ) ? "[{$info['cate']}]" : '';
+			$basisInfo	= ( $LOG_INFO & self::LOG_INFO_SERVICE ) ? "[{$info['service']}]" : '';
+			$moduleInfo	= ( $LOG_INFO & self::LOG_INFO_SERVICE ) ? "[{$info['module']}]" : '';
+			$routeInfo	= ( $LOG_INFO & self::LOG_INFO_ROUTE) ? "[{$info['route']}]" : '';
+			
+			
+			
+			$msg = "{$timeInfo}{$cateInfo}{$basisInfo}{$moduleInfo}{$routeInfo}{$tags} {$message}{$position}";
 			return $msg;
 		}
 		public function logMsg( $message, $logPos = FALSE, $logCate = '', $options = [] )
@@ -68,8 +90,8 @@
 			$logPath = DEFAULT_SYSTEM_LOG_DIR . "/" . (empty($logFileName) ? "error.pblog" : $logFileName);
 			$log	 = self::ObtainLog($logPath);
 
-			error_log( $msg = $log->genLogMsg( $message, $logPos, 'ERROR', $options ) );
-			return $log->logMsg( $msg, FALSE, '', [ 'raw-output' => TRUE ] );
+			error_log( $msg = $log->genLogMsg( $message, $logPos, 'ERROR', array_merge($options, [ 'info-level' => self::LOG_INFO_ALL & ~self::LOG_INFO_TIME ]) ) );
+			return $log->logMsg( $message, $logPos, 'ERROR', $options );
 		}
 		public static function SYSLog($message, $logPos = FALSE, $logFileName = '', $options = array())
 		{
