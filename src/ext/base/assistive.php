914cad96f3a759b32136bc9a142f683d3cd56d99
JCloudYu <j.cloud.yu@gmail.com>
2017-02-11 14:30:34 +0800

    Detach PBDataTree and PBVarVault from kernel

--- src/ext/base/assistive.php
+++ src/ext/base/assistive.php
@@ -1,12 +1,11 @@
 <?php
-/**
- * 1004.IMSLS - assitive.php
- * Created by JCloudYu on 2014/06/22 00:38
- */
+	using( 'sys.tool.PBVarVault' );
+	using( 'sys.tool.PBDataTree' );
+	
+	
 
 	// INFO: System-wide shared variable container (PBVariable)
-	function V()
-	{
+	function V() {
 		static $_singleton = NULL;
 		if ($_singleton) return $_singleton;
 
@@ -14,18 +13,25 @@
 		return $_singleton;
 	}
 
-	// INFO: System-wide and hierachical data storage (PBDataTree)
-	function& D( $selector = NULL, $value = NULL )
-	{
+	// INFO: System-wide and hierarchical data storage (PBDataTree)
+	function& D( $selector = NULL, $value = NULL ) {
 		static $_singleton = NULL;
 		if ( $_singleton === NULL ) $_singleton = new PBDataTree();
-
 
 		$numArgs = func_num_args();
 		if ( $numArgs == 0 ) return $_singleton;
 
-
 		return ( $numArgs > 1 ) ? $_singleton->set( $selector, $value ) : $_singleton->get( $selector );
 	}
 
-	function S( $selector = NULL ) { return D( $selector ); } // DEPRECATED: S will be deprecated in v2.0.0
+
+
+
+
+	
+	// region [ Deprecated ]
+	function S( $selector = NULL ) {
+		DEPRECATION_WARNING( "Function S is marked as deprecated! Please refer to D instead!" ); 
+		return D( $selector );
+	}
+	// endregion
