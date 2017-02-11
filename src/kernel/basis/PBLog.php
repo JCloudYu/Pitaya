ad3eec21527c83c797185d30d00b6db64fb8861a
JCloudYu <j.cloud.yu@gmail.com>
2017-02-11 13:49:16 +0800

    Rewrite PBLog api
    1. directly deprecates PBLog::LogDB / PBLog::ForceDB / PBLog::ConnectDB apis
    2. Minor modifications

--- src/kernel/basis/PBLog.php
+++ src/kernel/basis/PBLog.php
@@ -16,18 +16,16 @@
 		const LOG_INFO_ALL		= 0xFFFFFFFF;
 	
 		private $_logStream = NULL;
-		public function __construct($logPath)
-		{
+		public function __construct($logPath) {
 			$this->_logStream = self::ObtainStream($logPath);
 		}
-		public function genLogMsg( $message, $logPos = FALSE, $logCate = '', $options = [] )
-		{
+		public function genLogMsg( $message, $logPos = FALSE, $logCate = '', $options = [] ) {
 			if ( !is_array($options) ) $options = [];
 			
 			$LOG_INFO = array_key_exists( 'info-level', $options ) ? $options['info-level'] | 0 : self::LOG_INFO_ALL;
 			if ( !is_string($message) ) $message = print_r( $message, TRUE );
 			if ( !is_array(@$options['tags']) ) $options['tags'] = array();
-			$info = self::PrepLogInfo($logCate, $options);
+			$info = self::PrepLogInfo( $logCate );
 
 
 
@@ -58,21 +56,13 @@
 			$msg = "{$timeInfo}{$cateInfo}{$basisInfo}{$moduleInfo}{$routeInfo}{$tags} {$message}{$position}";
 			return $msg;
 		}
-		public function logMsg( $message, $logPos = FALSE, $logCate = '', $options = [] )
-		{
+		public function logMsg( $message, $logPos = FALSE, $logCate = '', $options = [] ) {
 			if ( empty($this->_logStream) ) return FALSE;
 
 
 			$msg = ( @$options[ 'row-output' ] === TRUE ) ? $message : $this->genLogMsg( $message, $logPos, $logCate, $options );
 			fwrite( $this->_logStream, "{$msg}\n" );
 			fflush( $this->_logStream );
-
-			if (!empty(PBLog::$LogDB))
-			{
-				if (PBLog::$_forceLogDB || in_array('WRITE_DB', $options))
-					PBLog::LogDB("{$message}{$position}", $info);
-			}
-
 			return $msg;
 		}
 
@@ -117,108 +107,17 @@
 
 
 
-		// INFO: Global logging API
-		/**
-		 * @var PDO
-		 */
-		private static $LogDB	= NULL;
-		private static $LogTbl	= '';
-
-		const LOG_TABLE = '__ext_pdo_sys_wide_log';
-
-		public static function ConnectDB($conInfo = array())
+		public static function ObtainLog($logFilePath)
 		{
-			if (PBLog::$LogDB !== NULL) return;
+			static $_cachedLog = array();
 
-			$driver = empty($conInfo['type']) ? 'mysql' : "{$conInfo['type']}";
-			$dsn	= "{$driver}:host={$conInfo['host']};port={$conInfo['port']};dbname={$conInfo['db']};";
+			$pathKey = md5($logFilePath);
+			if (empty($_cachedLog[$pathKey]))
+				$_cachedLog[$pathKey] = new PBLog($logFilePath);
 
-			PBLog::$LogDB = new PDO($dsn, "{$conInfo['account']}", "{$conInfo['password']}");
-			PBLog::$LogDB->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-			PBLog::$LogDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-			PBLog::$LogTbl = empty($conInfo['table']) ? PBLog::LOG_TABLE : $conInfo['table'];
-
-
-			$tbl = PBLog::$LogTbl;
-			$checkTbl = PBLog::$LogDB->query("SHOW TABLES LIKE '{$tbl}';")->fetch();
-			if (empty($checkTbl))
-			{
-				PBLog::$LogDB->query(<<<SQL
-					CREATE TABLE IF NOT EXISTS `{$tbl}` (
-						`id` int(11) NOT NULL AUTO_INCREMENT,
-						`cate` varchar(128) NOT NULL,
-						`service` varchar(128) NOT NULL,
-						`module` varchar(128) NOT NULL,
-						`tags` text NOT NULL,
-						`route` text NOT NULL,
-						`msg` longtext NOT NULL,
-						`time` bigint(20) NOT NULL,
-						PRIMARY KEY (`id`)
-					) DEFAULT CHARSET=utf8;
-SQL
-				);
-			}
+			return $_cachedLog[$pathKey];
 		}
-
-		// cate time service module tag route msg
-		private static function LogDB($message, array $attributes = array())
-		{
-			if (empty(PBLog::$LogDB)) return FALSE;
-
-			$tableName = PBLog::$LogTbl;
-			$stmt = PBLog::$LogDB->prepare("INSERT INTO `{$tableName}`(`cate`, `service`, `module`, `tags`, `route`, `msg`, `time`)
-								  								VALUES(:cate, :service, :module, :tags, :route, :msg, :time);");
-
-			$stmt->execute(array(
-				':cate'	=> @"{$attributes['cate']}",
-				':service'	=> @"{$attributes['service']}",
-				':module'	=> @"{$attributes['module']}",
-				':tags'	=> @"{$attributes['tags']}",
-				':route'	=> @"{$attributes['route']}",
-				':msg'		=>  "{$message}",
-				':time'	=> @"{$attributes['time']}"
-			));
-
-			return $stmt->rowCount() > 0;
-		}
-
-		private static $_forceLogDB = FALSE;
-		public static function ForceDB($force = FALSE) { self::$_forceLogDB = !empty($force); }
-
-
-		/**
-		 * Produce environmental information
-		 *
-		 * @param string $logCate
-		 * @param array $options
-		 *
-		 * @return array
-		 */
-		private static function PrepLogInfo($logCate = '', $options = array())
-		{
-			static $remoteIp = NULL;
-
-			// INFO: Collect remote ip
-			if ($remoteIp === NULL)
-				$remoteIp = RemoteIP(PBRequest::Request()->server, TRUE, TRUE);
-
-			// INFO:
-			if (SYS_WORKING_ENV === SYS_ENV_CLI)
-			{
-				$route = 'CLI';
-
-				if (function_exists("posix_geteuid") && function_exists("posix_getpwuid"))
-				{
-					$shellInfo = posix_getpwuid(posix_getuid());
-					$route = "CLI({$shellInfo['name']}|{$shellInfo['uid']}|{$shellInfo['gid']})";
-				}
-			}
-			else
-				$route = $remoteIp;
-
-
+		private static function PrepLogInfo( $logCate = '' ) {
 
 			$trace = debug_backtrace();
 			array_shift($trace);	// This scope
@@ -244,36 +143,10 @@ SQL
 				'timestamp' => date("Y-m-d G:i:s", $curTime),
 				'service'	=> (!defined('__SERVICE__') ? 'Pitaya' : __SERVICE__),
 				'module'	=> $module,
-				'route'		=> $route,
+				'route'		=> (SYS_WORKING_ENV === SYS_ENV_CLI) ? 'CLI' : 'NET',
 				'position'	=> "{$trace[0]['file']}:{$trace[0]['line']}"
 			);
 		}
-
-		/**
-		 * Obtain log singleton according to log file path
-		 *
-		 * @param string $logFilePath
-		 *
-		 * @return PBLog
-		 */
-		public static function ObtainLog($logFilePath)
-		{
-			static $_cachedLog = array();
-
-			$pathKey = md5($logFilePath);
-			if (empty($_cachedLog[$pathKey]))
-				$_cachedLog[$pathKey] = new PBLog($logFilePath);
-
-			return $_cachedLog[$pathKey];
-		}
-
-		/**
-		 * Obtain log stream according to log file path
-		 *
-		 * @param string $logFilePath
-		 *
-		 * @return Resource
-		 */
 		private static function ObtainStream($logFilePath)
 		{
 			static $_fileStream = array();
