96b660b86f4f8b76ad35d52b37f2a3a5908689b7
JCloudYu <j.cloud.yu@gmail.com>
2017-02-22 23:36:56 +0800

    Start replacing bash with php cli mode

---
+++ shell/help/boot.php
@@ -0,0 +1,7 @@
+<?php
+	fwrite( STDERR, <<<TEXT
+Usage: pitaya <command> [args...]
+       pitaya <path_to_the_vector> [args...]
+       pitaya [args...] (The working directory must be an available pitaya space)\n
+TEXT
+);
---
+++ shell/pitaya.php
@@ -0,0 +1,52 @@
+#!/usr/bin/env php
+<?php
+	$LIB_PATH	 = realpath(__DIR__ . '/../');
+	$PORTAL_PATH = "{$LIB_PATH}/src/portal.php";
+	
+	if ( !is_file("{$PORTAL_PATH}") || !is_readable("{$PORTAL_PATH}") ) {
+		fwrite( STDERR, "Incompatible Execution Environment!" . PHP_EOL );
+		exit( 1 );
+	}
+	
+	
+
+	$WORKING_DIR = realpath(getcwd());
+	$ARGV = empty($_SERVER[ 'argv' ]) ? [] : $_SERVER[ 'argv' ];
+	array_shift( $ARGV ); // remove script path
+	
+	switch( $mode = @$ARGV[0] ) {
+		case "-k":
+			require "{$LIB_PATH}/shell/kernel/boot.php";
+			return;
+			
+		case "-c":
+			require "{$LIB_PATH}/shell/command/boot.php";
+			return;
+			
+		case "-h":
+			require "{$LIB_PATH}/shell/help/boot.php";
+			return;
+	}
+
+
+
+	if ( is_file($mode) && is_readable($mode) ) { // INFO: Vector Mode
+		array_shift($ARGV);
+		$mode = escapeshellarg($mode);
+		$args = []; foreach( $ARGV as $arg ) $args[] = escapeshellarg($arg);
+		$proc = proc_open( "php {$LIB_PATH}/src/portal.php -entry {$mode}" . implode( ' ',  $args ), [ STDIN, STDOUT, STDERR ], $pipes );
+		$stat = proc_close($proc);
+		exit( $stat );
+	}
+
+	$gateway = "{$WORKING_DIR}/gateway.php";
+	if ( is_file("{$gateway}") ) {
+		$args = []; foreach( $ARGV as $arg ) $args[] = escapeshellarg($arg);
+		$proc = proc_open( "php ./gateway.php " . implode( ' ',  $args ), [ STDIN, STDOUT, STDERR ], $pipes );
+		$stat = proc_close($proc);
+		exit( $stat );
+	}
+
+
+	
+	require "{$LIB_PATH}/shell/help/boot.php";
