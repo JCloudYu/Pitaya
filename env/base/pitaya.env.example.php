ff919f75b6bd15a43697f814cc892e7770b187e2
JCloudYu <j.cloud.yu@gmail.com>
2017-02-23 02:10:50 +0800

    Use Basis as standard basis directory name

--- env/base/pitaya.env.example.php
+++ env/base/pitaya.env.example.php
@@ -1,5 +1,5 @@
 <?php
 	define( '__PITAYA_PATH',	ROOT . '/Pitaya'	);
-	define( '__BASIS_PATH',		ROOT . '/Services'	);
+	define( '__BASIS_PATH',		ROOT . '/Basis'		);
 	define( '__SHARE_PATH',		ROOT . '/Share'		);
 	define( '__DATA_PATH',		ROOT . '/Data'		);
