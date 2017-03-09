0d58568df7d568e5b3eccb91604b22239d7aca21
JCloudYu <j.cloud.yu@gmail.com>
2017-03-09 22:05:14 +0800

    Add template path argument to allow user to sepecify custom paths

--- src/kernel/extension/PBTmplRenderer.php
+++ src/kernel/extension/PBTmplRenderer.php
@@ -4,22 +4,25 @@
 		public static function SetTplPath( $path ) {
 			self::$_tplPath = $path;
 		}
-		public static function Tpl( $tmplName ){
-			return new PBTmplRenderer( $tmplName );
+		public static function Tpl( $tmplName, $basePath = NULL ){
+			$tmplPath = func_num_args() > 1 ? $basePath : self::$_tplPath;
+			return new PBTmplRenderer( $tmplName, $tmplPath );
 		}
 	
 
 		
+		private $_tplBasePath = "";
 		private $_tplName = "";
-		private function __construct( $tmplName ) {
+		private function __construct( $tmplName, $basePath ) {
 			$this->_tplName = $tmplName;
+			$this->_tplBasePath = $basePath;
 		}
 		public function __toString() {
 			return $this();
 		}
 		public function __invoke( $output = FALSE ) {
 			$path = str_replace( '.', '/', $this->_tplName );
-			$scriptPath = self::$_tplPath . "/{$path}.php";
+			$scriptPath = "{$this->_tplBasePath}/{$path}.php";
 			if (!$output) ob_start();
 			$this->render( $scriptPath, $this->_variables );
 			return (!$output) ? ob_get_clean() : "";
