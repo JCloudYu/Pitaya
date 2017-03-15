d4be61d94714662596ccbda8750d9e166c4f5d07
JCloudYu <j.cloud.yu@gmail.com>
2017-03-15 14:33:48 +0800

    Rewrite using latest supportive api

--- src/modules/PBOutputCtrl.php
+++ src/modules/PBOutputCtrl.php
@@ -405,7 +405,7 @@
 			$param = clone $param;
 			$ajaxReturn->status = CAST( @$param->status, 'int strict',	self::STATUS_NORMAL );
 			$ajaxReturn->msg	= CAST( @$param->msg,	 'string',		'' );
-			$ajaxReturn->scope	= PBScopeTracer::Scope()->breadcrumb( '#' );
+			$ajaxReturn->scope	= PBScope()->breadcrumb( '#' );
 			$ajaxReturn			= data_set( $ajaxReturn, $param );
 
 			return $ajaxReturn;
