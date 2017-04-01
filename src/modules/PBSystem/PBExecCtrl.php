24af2882a7da70ad35f8cc27c7ff16bd0bf025d0
JCloudYu <j.cloud.yu@gmail.com>
2017-04-02 00:21:53 +0800

    Remove redundant initData argument

--- src/modules/PBSystem/PBExecCtrl.php
+++ src/modules/PBSystem/PBExecCtrl.php
@@ -1,25 +1,25 @@
 <?php
-	class PBBasisChain extends PBModule
-	{
+	class PBBasisChain extends PBModule {
 		private $_chainInfo = array();
 		public function __construct( $chainInfo ) {
 			$this->_chainInfo = is_array($chainInfo) ? $chainInfo : [];
 		}
 		
-		public function execute( $chainData = NULL, $request = NULL ) {
-			foreach ( $this->_chainInfo as $chainModule )
-				$this->chain[] = [ 'module' => $chainModule, 'request' => $request ];
+		public function execute( $chainData ) {
+			foreach ( $this->_chainInfo as $chainModule ) {
+				$this->chain[] = $module = PBModule($chainModule);
+				data_fuse($module->data, $this->data);
+			}
 				
 			return $chainData;
 		}
 	}
 
-	class PBVectorChain extends PBModule
-	{
-		public function execute( $chainData = NULL, $moduleRequest = NULL )
-		{
+	class PBVectorChain extends PBModule {
+		public function execute( $chainData ) {
 			$module			= PBModule( "working." . __STANDALONE_MODULE__ );
-			$this->chain[]	= [ 'module' => $module, 'request' => $moduleRequest ];
+			$this->chain[]	= PBModule($module);
+			data_fuse($module->data, $this->data);
 			
 			return $chainData;
 		}
