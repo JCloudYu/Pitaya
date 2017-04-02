8b2a8ad41f4bec8fe1473487282b7674963e81ab
JCloudYu <j.cloud.yu@gmail.com>
2017-04-02 19:59:27 +0800

    Rewrite PBDataTree object and make it be the core object

---
+++ src/kernel/basis/PBDataTree.php
@@ -0,0 +1,60 @@
+<?php
+	class PBDataTree implements ArrayAccess {
+		private $_anchor = NULL;
+		
+		public function __construct(stdClass $anchorObj = NULL) {
+			$this->_anchor = $anchorObj ?: stdClass();
+		}
+		public function travel( $path ) {
+			$comps = explode( '.', $path );
+			$currObj = NULL;
+			while( !empty($comps) ) {
+				$item = array_shift($comps);
+				if ( !is_a($comps->{$item}, stdClass::class) ) {
+					$comps->{$item} = stdClass();
+				}
+				
+				$currObj = $comps->{$item};
+			}
+			
+			return PBDataTree($currObj);
+		}
+		
+		
+		
+		public function& __get($name) {
+			if ( is_a($this->_anchor->{$name}, stdClass::class) ) {
+				return PBDataTree($this->_anchor->{$name});
+			}
+			
+			return $this->_anchor->{$name};
+		}
+		public function __set($name, $value) {
+			$this->_anchor->{$name} = $value;
+		}
+		public function __unset($name) {
+			unset($this->_anchor->{$name});
+		}
+		public function __isset($name) {
+			return property_exists($this->_anchor, $name);
+		}
+		
+		
+		
+		public function& offsetGet($offset) {
+			return $this->{$offset};
+		}
+		public function offsetSet($offset, $value) {
+			$this->{$offset} = $value;
+		}
+		public function offsetUnset($offset) {
+			unset($this->{$offset});
+		}
+		public function offsetExists($offset) {
+			return isset($this->{$name});
+		}
+	}
+	
+	function PBDataTree(stdClass $anchorObj = NULL) {
+		return new PBDataTree($anchorObj);
+	}
