abfe9b9b5eec92adde3dacbe26a5668f2c8ef862
JCloudYu <j.cloud.yu@gmail.com>
2017-04-10 00:07:15 +0800

    1. Mark PBLocale::basePackage api and property as deprecated 2. Add PBLocale::path property which allows developers to specify file system path rather than package path

--- src/sys/tool/PBLocale.php
+++ src/sys/tool/PBLocale.php
@@ -1,11 +1,7 @@
 <?php
 	using( 'sys.tool.PBScriptCtrl' );
 
-	final class PBLocale extends PBObject implements ArrayAccess {	
-		private static $_sharedBasePackage = NULL;
-		public static function basePackage($packagePath = NULL) { 
-			self::$_sharedBasePackage = "$packagePath";
-		}
+	final class PBLocale extends PBObject implements ArrayAccess {
 		public static function Locale() {
 			/** @var PBLocale $__locale_singleton */
 			static $__locale_singleton = NULL;
@@ -23,34 +19,36 @@
 
 
 
-		private $_basePackage 	= '';
+		
 		private $_curLocale 	= '';
 		private $_storedLocales = array();
 		public function __construct() {
-			$this->_basePackage = self::$_sharedBasePackage;
+			$this->_basePackage = path( self::$_sharedBasePackage );
+			
 			$this->_curLocale = 'default';
 			$this->_storedLocales = array();
 		}
 
 
 
-		public function __get_basePackage() {
-			return $this->_basePackage;
+		private $_localeLibPath = '';
+		public function __get_path() {
+			return $this->_localeLibPath;
 		}
-		public function __set_basePackage($value) {
-			$this->_basePackage = "$value";
+		public function __set_path($path) {
+			$this->_localeLibPath = $path;
 		}
+		
 		public function __get_locale() { 
 			return $this->_curLocale;
 		}
 		public function __set_locale($value) {
-			$this->_curLocale = empty($value) ? 'default' : "$value";
+			$this->_curLocale = empty($value) ? 'default' : "{$value}";
 			if ( !empty($this->_storedLocales[$this->_curLocale]) ) return;
 
 
 
-			$leading	= empty($this->_basePackage) ? 'service.' : "{$this->_basePackage}.";
-			$localePath	= path( "{$leading}{$this->_curLocale}", "locale.php" );
+			$localePath	= path( "{$this->_localeLibPath}/{$this->_curLocale}", "locale.php" );
 			if ( !is_file($localePath) || !is_readable($localePath) ) return;
 			
 			
@@ -74,8 +72,29 @@
 		public function offsetGet($offset) {
 			if ( is_array($this->_storedLocales[$this->_curLocale][ $offset ]) )
 				return $this->_storedLocales[$this->_curLocale][ $offset ];
-			return @strtr($offset, $this->_storedLocales[$this->_curLocale]);
+			return @strtr($offset, $this->_storedLocales[$this->_curLocale] ?: []);
 		}
+		
+		
+		
+		// region [ Deprecated ]
+		private static $_sharedBasePackage = NULL;
+		public static function basePackage($packagePath = NULL) {
+			DEPRECATION_WARNING( "PBLocale::basePackage api is marked as deprecated!" );
+			self::$_sharedBasePackage = "{$packagePath}";
+		}
+		
+		private $_basePackage = '';
+		public function __get_basePackage() {
+			DEPRECATION_WARNING( "PBLocale::basePackage property is marked as deprecated! Please refer to PBLocale::package property!" );
+			return $this->_basePackage;
+		}
+		public function __set_basePackage($value) {
+			DEPRECATION_WARNING( "PBLocale::basePackage property is marked as deprecated! Please refer to PBLocale::package property!" );
+			$this->_basePackage = "{$value}";
+			$this->_localeLibPath = path( $this->_basePackage );
+		}
+		// endregion
 	}
 	
 	function PBLocale(){
