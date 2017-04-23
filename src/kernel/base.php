ae55f02aa302f757418c61b1b263ff9f27e9f1ee
JCloudYu <j.cloud.yu@gmail.com>
2017-04-24 00:57:01 +0800

    1. Rename env.core.php into env.kernel.conf.php 2. Rearrange path dependency

--- src/kernel/base.php
+++ src/kernel/base.php
@@ -26,14 +26,15 @@
 	// INFO: Runtime configurations
    @include_once ROOT . ( IS_CLI_ENV ? "/cli.php" : "/config.php" );
    @include_once ROOT . "/common.php";
-
+	
 	require_once PITAYA_ROOT . '/kernel/env.native.php';
-	require_once PITAYA_ROOT . '/kernel/env.core.php';
+	require_once PITAYA_ROOT . '/kernel/env.kernel-conf.php';
 	require_once PITAYA_ROOT . '/kernel/env.path.php';
-	require_once PITAYA_ROOT . '/kernel/api.path.php';
-	require_once PITAYA_ROOT . '/kernel/env.time.php';
+	
 	require_once PITAYA_ROOT . '/kernel/env.runtime.php';
+	require_once PITAYA_ROOT . '/kernel/env.time.php';
 	require_once PITAYA_ROOT . "/kernel/env.const.php";
+	
 	
 
 	// INFO: Load system core libraries and prepare system constants
