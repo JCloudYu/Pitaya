07bce8eb56e26875893186af8f65683c6f15e59e
JCloudYu <j.cloud.yu@gmail.com>
2017-06-13 13:03:42 +0800

    Remove redundant package

--- src/kernel/prototype/PBCachePrototype.php
+++
@@ -1,6 +0,0 @@
-<?php
-	abstract class PBCachePrototype {
-		abstract public function store($token, $data);
-		abstract public function fetch($token);
-		abstract public function destroy($token);
-	}
