256fbd6fcebf777501ddb5670f7dd98795dc294c
JCloudYu <j.cloud.yu@gmail.com>
2017-06-13 12:54:02 +0800

    Remove legacy property PBObject::caller

--- src/kernel/basis/PBObject.php
+++ src/kernel/basis/PBObject.php
@@ -1,5 +1,4 @@
 <?php
-	// The class provides the native getter and setter functions
 	class PBObject {
 		private static $_getPrefix = "__get_";
 		private static $_setPrefix = "__set_";
@@ -34,40 +33,5 @@
 		public function __get_class() {
 
 			return get_class($this);
-		}
-		
-
-
-		protected function __get_caller() {
-			if ( !DEBUG_BACKTRACE_ENABLED ) return NULL;
-			DEBUG_WARNING( "PBObject::caller is designed for debugging! It can be harmful to your system performance" );
-
-			$tempBacktrace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT);
-			$tempBacktrace = array_reverse($tempBacktrace);
-
-			$backtrace = array();
-			$item = array_shift($tempBacktrace);
-			while( $item !== NULL)
-			{
-				$backtrace[] = $item;
-				if("{$item['function']}" == '__get' || "{$item['function']}" == '__set')
-				{
-					$item = array_shift($tempBacktrace);
-					if(preg_match('/^__set_|^__get_/', $item['function']) !== 1)
-					{
-						$backtrace[] = $item;
-					}
-				}
-
-				$item = array_shift($tempBacktrace);
-			}
-			$backtrace = array_reverse($backtrace);
-
-			array_shift($backtrace);
-
-			if(count($backtrace) <= 1)
-				return NULL;
-			else
-				return $backtrace[1];
 		}
 	}
