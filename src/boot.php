5bef46115b847fa12ffd6ee86ab4b79453efe33a
JCloudYu <j.cloud.yu@gmail.com>
2017-06-14 20:50:23 +0800

    Multiple improvements
    1. Rerrange framework's content topology
    2. Remove redundant constants
    3. Remove dependency to supportive library

--- src/boot.php
+++ src/boot.php
@@ -3,10 +3,15 @@
 	define( 'PITAYA_BASE_CORE_EXECUTED', TRUE );
 
 
-	define( 'PITAYA_METRIC_BOOT_TIME',		microtime( TRUE ) );
-	define( 'PITAYA_METRIC_BOOT_MEMORY',	memory_get_usage() );
-	require_once __DIR__ . "/kernel/env.version.php";
-	require_once __DIR__ . "/kernel/env.boot.php";
+	define( 'PITAYA_METRIC_BOOT_TIME', microtime(TRUE) );
+	define( 'PITAYA_ROOT', __DIR__ );
+	require_once PITAYA_ROOT . '/kernel/_env/env.native.php';
+	
+	
+	
+	
+	require_once PITAYA_ROOT . "/kernel/env.version.php";
+	require_once PITAYA_ROOT . "/kernel/env.boot.php";
 
 	// Detect minimum PHP Version
 	if ( PHP_VERSION_ID < 50600 ) {
@@ -18,7 +23,7 @@
 	}
 		
 		
-	if ( !defined( "ROOT" ) || !defined( "PITAYA_ROOT" ) ) {
+	if ( !defined( "ROOT" ) ) {
 		die( "Required system constants are missing ( ROOT, PITAYA_ROOT )!" );
 	}
 	
@@ -45,13 +50,7 @@
 	using( 'kernel.core.*' );
 	using( 'kernel.sys' );
 	
-	s_define( 'PITAYA_ADD_SUPPORTIVE_LIB', TRUE, TRUE, FALSE );
-	if ( PITAYA_ADD_SUPPORTIVE_LIB ) {
-		using( 'kernel.supportive.*' );
-	}
 	
 	
-	
-	require_once PITAYA_ROOT . "/kernel/env.cleanup.php";	
-	define( 'PITAYA_METRIC_KERNEL_TIME',	microtime( TRUE ) );
-	define( 'PITAYA_METRIC_KERNEL_MEMORY',	memory_get_usage() );
+	require_once PITAYA_ROOT . "/kernel/env.cleanup.php";
+	pb_metric();
