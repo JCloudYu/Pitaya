dbe72190c925b7b0affb142d121dc86dd09a2caf
JCloudYu <j.cloud.yu@gmail.com>
2017-06-15 00:15:19 +0800

    Make developers to handle the warnings themselves

--- src/kernel/env.runtime.php
+++ src/kernel/env.runtime.php
@@ -17,19 +17,6 @@
 			s_define( "DEFAULT_SYSTEM_LOG_DIR", sys_get_temp_dir(), TRUE );
 	});
 
-	// INFO: Runtime configurations
-	call_user_func(function(){
-		// INFO: Error Reporting Control
-		s_define( "PITAYA_SUPPRESS_EXPECTED_WARNINGS", FALSE, TRUE, FALSE );
-		error_reporting( PITAYA_SUPPRESS_EXPECTED_WARNINGS ? (E_ALL & ~E_STRICT & ~E_NOTICE) : E_ALL );
-		
-
-		set_error_handler(function( $errno, $errStr ){
-			if ( !PITAYA_SUPPRESS_EXPECTED_WARNINGS ) return FALSE;
-			return ( substr( $errStr, 0, 14 ) === 'Declaration of' );
-		}, E_WARNING );
-	});
-
 	s_define( 'DEBUG_CONSOLE_WIDTH', 350 );
 	s_define( 'DEBUG_MODE', FALSE );
 	s_define( 'THROWN_EXCEPTIONS', TRUE );
