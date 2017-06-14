2d41bbf6f11b6c42348bb513ebb762ddace3c610
JCloudYu <j.cloud.yu@gmail.com>
2017-06-15 00:17:58 +0800

    Minor modifications

--- src/kernel/env.runtime.php
+++ src/kernel/env.runtime.php
@@ -1,20 +1,25 @@
 <?php
 	call_user_func(function(){
-		if ( PITAYA_STANDALONE_EXECUTION_MODE && is_writable( path( $logPackage = 'working' )) )
+		if ( PITAYA_STANDALONE_EXECUTION_MODE && is_writable( path( $logPackage = 'working' )) ) {
 			s_define( "DEFAULT_SYSTEM_LOG_PACKAGE", $logPackage = "{$logPackage}.plog", TRUE );
+		}
 		else
-		if ( is_writable( path( $logPackage = 'data' )) )
+		if ( is_writable( path( $logPackage = 'data' )) ) {
 			s_define( "DEFAULT_SYSTEM_LOG_PACKAGE", $logPackage = "{$logPackage}.log", TRUE );
+		}
 		else
-		if ( is_writable( path( $logPackage = 'root' )) )
+		if ( is_writable( path( $logPackage = 'root' )) ) {
 			s_define( "DEFAULT_SYSTEM_LOG_PACKAGE", $logPackage, TRUE );
+		}
 		
 
 
-		if ( defined( 'DEFAULT_SYSTEM_LOG_PACKAGE' ) )
+		if ( defined( 'DEFAULT_SYSTEM_LOG_PACKAGE' ) ) {
 			s_define( "DEFAULT_SYSTEM_LOG_DIR",	path( DEFAULT_SYSTEM_LOG_PACKAGE ), TRUE );
-		else
+		}
+		else {
 			s_define( "DEFAULT_SYSTEM_LOG_DIR", sys_get_temp_dir(), TRUE );
+		}
 	});
 
 	s_define( 'DEBUG_CONSOLE_WIDTH', 350 );
