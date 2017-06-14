483b7e74f86917feef02e003ad2e6c8a63658e1d
JCloudYu <j.cloud.yu@gmail.com>
2017-06-15 00:03:28 +0800

    Minor modifications

--- src/kernel/env.kernel-conf.php
+++ src/kernel/env.kernel-conf.php
@@ -1,8 +1,7 @@
 <?php
-	// region [ Environmental Initialization ]
 	if ( IS_CLI_ENV ) {
-		// region [ CLI Initialization ]
 		array_shift( $_SERVER['argv'] ); // INFO: Remove script file path
+		
 		if ( @"{$_SERVER['argv'][0]}" == "-entry" ) {
 			array_shift($_SERVER['argv']);
 			$GLOBALS['STANDALONE_EXEC'] = array(
@@ -11,11 +10,8 @@
 			);
 			array_shift( $_SERVER['argv'] );
 			
-			define( 'PITAYA_STANDALONE_EXECUTION_MODE', TRUE );
+			s_define( 'PITAYA_STANDALONE_EXECUTION_MODE', TRUE );
 		}
-		$_SERVER['argc'] = count($_SERVER['argv']);
-		// endregion
 	}
 	
-	if ( !defined( 'PITAYA_STANDALONE_EXECUTION_MODE' ) ) define( 'PITAYA_STANDALONE_EXECUTION_MODE', FALSE );
-	// endregion
+	s_define( 'PITAYA_STANDALONE_EXECUTION_MODE', FALSE );
