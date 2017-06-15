ccd3c7e6fb4c0b018a85612a73192399ddfc6484
JCloudYu <j.cloud.yu@gmail.com>
2017-06-15 14:08:39 +0800

    Multiple improvements
    1. Remove unnecessary constants
    2. Remove redundant function definition

--- env/base/gateway.php
+++ env/base/gateway.php
@@ -1,42 +1,37 @@
 <?php
 	error_reporting( E_ALL & ~E_NOTICE );
-
 	define( 'ROOT', realpath(dirname("{$_SERVER['SCRIPT_FILENAME']}")) );
-	define( '__IS_CLI_ENV', php_sapi_name() == 'cli' );
-	define( '__IS_WIN_ENV', (strtoupper(substr( PHP_OS, 0, 3 )) === 'WIN') );
-	function resolveLnk( $lnkPath ) {
-		$lnkPath  = realpath($lnkPath);
-		$shell = new COM('WScript.Shell');
-		$shortcut = $shell->createshortcut($lnkPath);
-		$targetPath = $shortcut->targetpath;
-		return $targetPath;
-	
-		/*
-			// The following method could be failed on COM generated lnk files
-			// Borrowed from http://www.witti.ws/blog/2011/02/21/extract-path-lnk-file-using-php
-			$linkContent = file_get_contents( $lnkPath );
-			return preg_replace( '@^.*\00([A-Z]:)(?:[\00\\\\]|\\\\.*?\\\\\\\\.*?\00)([^\00]+?)\00.*$@s', '$1\\\\$2', $linkContent );
-		*/
-	}
 	
 	
 	
    @include_once ROOT . "/pitaya.env.php";
-	$pitayaRootPath = defined( '__PITAYA_PATH' ) ? __PITAYA_PATH : ROOT . '/Pitaya';
-	if ( __IS_WIN_ENV && !is_dir( $pitayaRootPath ) && is_file( "{$pitayaRootPath}.lnk" ) ) {
-		$pitayaRootPath = resolveLnk( "{$pitayaRootPath}.lnk" );
-	}
-	$pitayaRootPath = realpath($pitayaRootPath);
+	call_user_func(function(){
+		$isWinEnv = (strtoupper(substr( PHP_OS, 0, 3 )) === 'WIN');
 	
-	define( '__PITAYA_ROOT', $pitayaRootPath );
+		$pitayaRootPath = defined( '__PITAYA_PATH' ) ? __PITAYA_PATH : ROOT . '/Pitaya';
+		if ( $isWinEnv ) {
+			if ( is_file("{$pitayaRootPath}.lnk") ) {
+				$lnkPath  = realpath("{$pitayaRootPath}.lnk");
+				$shell = new COM('WScript.Shell');
+				$shortcut = $shell->createshortcut($lnkPath);
+				$pitayaRootPath = $shortcut->targetpath;
+			}
+		}
+		$pitayaRootPath = realpath($pitayaRootPath);
+		
+		define( '__PITAYA_LIB_PATH', $pitayaRootPath );
+	});
+	
+	
+   
 
 
 	
 	// INFO: Runtime configurations
    @include_once ROOT . '/pitaya.php';
-   @include_once ROOT . ( __IS_CLI_ENV ? "/cli.php" : "/config.php" );
+   @include_once ROOT . ( (php_sapi_name() == 'cli') ? "/cli.php" : "/config.php" );
    @include_once ROOT . "/common.php";
 	
 	
 	
-	require_once __PITAYA_ROOT . "/portal.php";
+	require_once __PITAYA_LIB_PATH . "/portal.php";
