f24bd88472c13ef33ba848c4fb320f4cb4c23923
JCloudYu <j.cloud.yu@gmail.com>
2017-06-15 14:02:20 +0800

    Make things work...

--- env/base/gateway.php
+++ env/base/gateway.php
@@ -2,8 +2,8 @@
 	error_reporting( E_ALL & ~E_NOTICE );
 
 	define( 'ROOT', realpath(dirname("{$_SERVER['SCRIPT_FILENAME']}")) );
-	define( '__SPACE_ROOT__', ROOT ); // DEPRECATED: __SPACE_ROOT__ will be removed in 2.5.0
-	define( 'IS_WIN_ENV', (strtoupper(substr( PHP_OS, 0, 3 )) === 'WIN') );
+	define( '__IS_CLI_ENV', php_sapi_name() == 'cli' );
+	define( '__IS_WIN_ENV', (strtoupper(substr( PHP_OS, 0, 3 )) === 'WIN') );
 	function resolveLnk( $lnkPath ) {
 		$lnkPath  = realpath($lnkPath);
 		$shell = new COM('WScript.Shell');
@@ -23,17 +23,20 @@
 	
    @include_once ROOT . "/pitaya.env.php";
 	$pitayaRootPath = defined( '__PITAYA_PATH' ) ? __PITAYA_PATH : ROOT . '/Pitaya';
-	if ( IS_WIN_ENV && !is_dir( $pitayaRootPath ) && is_file( "{$pitayaRootPath}.lnk" ) ) {
+	if ( __IS_WIN_ENV && !is_dir( $pitayaRootPath ) && is_file( "{$pitayaRootPath}.lnk" ) ) {
 		$pitayaRootPath = resolveLnk( "{$pitayaRootPath}.lnk" );
 	}
 	$pitayaRootPath = realpath($pitayaRootPath);
 	
 	define( '__PITAYA_ROOT', $pitayaRootPath );
-	define( '__ROOT__', __PITAYA_ROOT ); // DEPRECATED: __ROOT__ will be removed in 2.5.0
-	require_once __PITAYA_ROOT . "/portal.php";
 
 
+	
 	// INFO: Runtime configurations
    @include_once ROOT . '/pitaya.php';
-   @include_once ROOT . ( IS_CLI_ENV ? "/cli.php" : "/config.php" );
+   @include_once ROOT . ( __IS_CLI_ENV ? "/cli.php" : "/config.php" );
    @include_once ROOT . "/common.php";
+	
+	
+	
+	require_once __PITAYA_ROOT . "/portal.php";
