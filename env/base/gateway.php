259bb552b3f48159b769f3f1f72e3fa4946a3487
JCloudYu <j.cloud.yu@gmail.com>
2017-07-11 00:17:46 +0800

    Adapt to the latest kernel modifications

--- env/base/gateway.php
+++ env/base/gateway.php
@@ -28,36 +28,6 @@
 	
    @include_once __ROOT . "/pitaya.config.php";
 	require_once __PITAYA_LIB_PATH . '/pitaya.php';
+	require_once __PITAYA_LIB_PATH . '/init-full.php';
 	
-	
-	
-	Pitaya([
-		'space-root' => __ROOT,
-		'packages' => [
-			'broot'		=> @constant( '__BASIS_PATH' ) ?: __ROOT.'/Basis',
-			'share' 	=> @constant( '__SHARE_PATH' ) ?: __ROOT.'/Share',
-			'data'		=> @constant( '__DATA_PATH' ) ?: __ROOT.'/Data',
-			'lib'		=> @constant( '__LIB_PATH' )  ?: __ROOT.'/Lib'
-		],
-		'module-packages' => [
-			'data.modules', 'share.modules'
-		],
-		'boot-scripts' => [
-			'broot.boot', 'share.boot', 'root.boot'
-		],
-		'default-basis'	=> (php_sapi_name() == 'cli') ? 'cli' : 'index',
-		'debug-mode' => @constant( 'DEBUG_MODE' ),
-		'debug-console-width' => @constant( 'DEBUG_CONSOLE_WIDTH' ),
-		'system-timezone' => @constant( 'SYS_TIMEZONE' ),
-		'throw-exceptions' => @constant( 'THROW_EXCEPTIONS' ),
-		'log-exceptions' => @constant( 'LOG_EXCEPTIONS' ),
-		'attach-depth' => @constant( 'ENV_ATTACH_DEPTH' ) ?: @constant( 'PITAYA_ENVIRONMENTAL_ATTACH_LEVEL' ),
-		
-		
-		'leading-modules' => [],
-		'tailing-modules' => [],
-		'log-dir' => __ROOT . '/Log'
-	]);
-	
-//	require_once __PITAYA_LIB_PATH . "/boot.php";
-//	PBKernel::boot();
+	Pitaya()->execute();
