eef25d2f60d87c47292c940bcc6f15eaa0e9d3f9
JCloudYu <j.cloud.yu@gmail.com>
2017-07-11 00:28:47 +0800

    Adapt to kernel's latest modification

--- env/base/pitaya.example.php
+++ env/base/pitaya.example.php
@@ -2,12 +2,12 @@
 //	define( 'ENV_ATTACH_DEPTH', 	0 );		// Default: 0
 //	define( "SYS_TIMEZONE",			"UTC" );	// Default: UTC
 //	define( "DEBUG_MODE",			TRUE );		// Default: FALSE
-//	define( "THROW_EXCEPTIONS",	FALSE );	// Default: TRUE
+//	define( "THROW_EXCEPTIONS",		FALSE );	// Default: TRUE
 //	define( "LOG_EXCEPTIONS",		FALSE );	// Default: TRUE
-//	define( "DEBUG_CONSOLE_WIDTH",	350 );		// Default: 350
+//	define( "DEBUG_DIALOG_WIDTH",	350 );		// Default: 350
 	
 //	define( '__PITAYA_PATH', __ROOT.'/Pitaya'	);
 //	define( '__BASIS_PATH',	 __ROOT.'/Basis'	);
 //	define( '__SHARE_PATH',	 __ROOT.'/Share'	);
-//	define( '__DATA_PATH',	 __ROOT.'/Data'	);
-//	define( '__LIB_PATH',	 __ROOT.'/Lib'	);
+//	define( '__DATA_PATH',	 __ROOT.'/Data'		);
+//	define( '__LIB_PATH',	 __ROOT.'/Lib'		);
