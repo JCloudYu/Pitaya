6b9c287705dcd2c92f3e3f7a449b803ce2800e71
JCloudYu <j.cloud.yu@gmail.com>
2017-07-11 01:14:09 +0800

    Lowercase default execution dependencies' names

--- env/base/pitaya.example.php
+++ env/base/pitaya.example.php
@@ -1,13 +1,14 @@
 <?php
-//	define( 'ENV_ATTACH_DEPTH', 	0 );		// Default: 0
-//	define( "SYS_TIMEZONE",			"UTC" );	// Default: UTC
-//	define( "DEBUG_MODE",			TRUE );		// Default: FALSE
-//	define( "THROW_EXCEPTIONS",		FALSE );	// Default: TRUE
-//	define( "LOG_EXCEPTIONS",		FALSE );	// Default: TRUE
-//	define( "DEBUG_DIALOG_WIDTH",	350 );		// Default: 350
-	
-//	define( '__PITAYA_PATH', __ROOT.'/Pitaya'	);
-//	define( '__BASIS_PATH',	 __ROOT.'/Basis'	);
-//	define( '__SHARE_PATH',	 __ROOT.'/Share'	);
-//	define( '__DATA_PATH',	 __ROOT.'/Data'		);
-//	define( '__LIB_PATH',	 __ROOT.'/Lib'		);
+//	const ENV_ATTACH_DEPTH		= 0;				// Default: 0
+//	const SYS_TIMEZONE			= 'UTC';			// Default: UTC
+//	const DEBUG_MODE			= FALSE;			// Default: FALSE
+//	const THROW_EXCEPTIONS		= FALSE;			// Default: FALSE
+//	const LOG_EXCEPTIONS		= TRUE;				// Default: TRUE
+//	const DEBUG_DIALOG_WIDTH	= 350;				// Default:350
+//	const SYS_LOG_DIR			= __ROOT . '/log';
+
+//	const PITAYA_PATH			= __ROOT . '/pitaya';
+//	const BASIS_PATH			= __ROOT . '/basis';
+//	const SHARE_PATH			= __ROOT . '/share';
+//	const DATA_PATH				= __ROOT . '/data';
+//	const LIB_PATH				= __ROOT . '/lib';
